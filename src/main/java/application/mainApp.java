fe9b05c520ccd92043aeb66c2f966430f2087269
IanIwanczuk <ian.iwn2003@gmail.com>
2025-01-14 12:35:49 +0100

    hibernate.cfg

--- src/main/java/application/mainApp.java
+++ src/main/java/application/mainApp.java
@@ -5,12 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
